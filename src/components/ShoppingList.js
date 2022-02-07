57536060e465eee614d39f9e627109aec780d065
Francois Spies <fcspies@gmail.com>
2022-02-07 18:37:35 -0500

    commit tree 6570559e428cc3ae7129925dd69e3c9f5690e805

--- src/components/ShoppingList.js
+++ src/components/ShoppingList.js
@@ -1,11 +1,25 @@
-import React from "react";
+import React, {useState} from "react";
 import Item from "./Item";
 
 function ShoppingList({ items }) {
+
+  const [selectedCategory, setSelectedCategory] = useState('items')
+
+  function handleChange(e) {
+    console.log(e.target.value)
+    setSelectedCategory(e.target.value)
+  }
+  
+  console.log('Sel Cat', selectedCategory)
+
+  const filteredState = items.filter(item => selectedCategory === 'items'? true : item.category === selectedCategory)
+
+  console.log(filteredState)
+
   return (
     <div className="ShoppingList">
       <div className="Filter">
-        <select name="filter">
+        <select name="filter" onChange={handleChange}>
           <option value="All">Filter by category</option>
           <option value="Produce">Produce</option>
           <option value="Dairy">Dairy</option>
@@ -13,8 +27,8 @@ function ShoppingList({ items }) {
         </select>
       </div>
       <ul className="Items">
-        {items.map((item) => (
-          <Item key={item.id} name={item.name} category={item.category} />
+        {filteredState.map((item) => (
+          <Item key={item.id} name={item.name} category={item.category}/>
         ))}
       </ul>
     </div>
